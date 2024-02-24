dc9631fe0cae247595143b09b54c0ce2a2c983d4
Jackman108 <dev.js.eugene@gmail.com>
2024-02-24 03:33:27 +0300

    Create App

---
+++ controllers/SiteController.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\authclient\ClientInterface;
+use Yii\base\Exception;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors(): array
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+
+    public function actions(): array
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex(): string
+    {
+        return $this->render('index');
+    }
+
+    /**
+     * Login action.
+     *
+     * @return Response|string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout(): Response
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    /**
+     * Displays contact page.
+     *
+     * @return Response|string
+     */
+    public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        }
+        return $this->render('contact', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Displays about page.
+     *
+     * @return string
+     */
+    public function actionAbout(): string
+    {
+        return $this->render('about');
+    }
+
+    /**
+     * OAuth2 authentication action.
+     *
+     * @return Response
+     * @throws Exception
+     */
+
+    public function actionAuth(): Response
+    {
+        $authClient = Yii::$app->authClientCollection->getClient('google');
+        $redirectUri = Yii::$app->urlManager->createAbsoluteUrl(['site/auth']);
+
+        if (!$authClient instanceof ClientInterface) {
+            throw new yii\base\Exception('Unknown auth client');
+        }
+
+        if (isset($_GET['code'])) {
+            $token = $authClient->fetchAccessToken($_GET['code']);
+            $attributes = $authClient->getUserAttributes();
+            return $this->redirect(['site/index']);
+        } else {
+            $authUrl = $authClient->buildAuthUrl(['redirect_uri' => $redirectUri]);
+            return $this->redirect($authUrl);
+        }
+    }
+}
